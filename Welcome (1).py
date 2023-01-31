96327afde76526109fc8074d80728fcac1deb0af
eric.vogelpohl@outlook.com <eric.vogelpohl@outlook.com>
2023-01-31 00:30:07 +0000

    edts

---
+++ Welcome (1).py
@@ -0,0 +1,55 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC 
+# MAGIC <img src = 'https://raw.githubusercontent.com/BlueprintTechnologies/Databricks_Sql_Workshop/main/img_artifacts/header.png' >
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## 1 Lakehouse Architecture & Familiarity with the Web UI (15 Minutes)
+# MAGIC 
+# MAGIC 
+# MAGIC * Reference Architecture (here)
+# MAGIC * Blueprint's Focus on Cost Transparency | Lakehouse Monitor (here)
+# MAGIC * Explore the SQL Workspace UI
+# MAGIC * View the Data Explorer UI
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## 2 SQL Data Warehouse Clustering (15 Minutes)
+# MAGIC * Clusters: Create & Understand the role of sizing. 
+# MAGIC * Discuss the new Serverless option for Cluster.
+# MAGIC * Permissions & Access
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 3 Hands-on Lab. (90 Minutes)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Datasets & Tables
+# MAGIC |   |   |
+# MAGIC |---|---|
+# MAGIC | <b>NYC taxi: Yellow taxi trips + Lat/Long (835M rows)</b><br><br>  &bullet; Source: https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page<br><br><b>Bureau of Transportation - On Time Flight Performance (79M rows)</b><br><br>  &bullet; Source: https://www.transtats.bts.gov/ontime/<br><br><b>Iowa State Liquor Sales - All state liquore store sales from 2012 to present</b><br><br>  &bullet; Source: https://data.iowa.gov/stories/s/Iowa-Liquor-Sales-Explorer/mke2-7r5k |  <img src="https://raw.githubusercontent.com/BlueprintTechnologies/Databricks_Sql_Workshop/main/img_artifacts/tables.png" /> |
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Lab Setup & Access:
+# MAGIC * Link provided at the workshop
+# MAGIC * Ids & passwords (provided at the workshop)
+# MAGIC * Navigate to the SQL Workspace
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Experience Challenges Data Exploration
+# MAGIC |   |   |
+# MAGIC |---|---|
+# MAGIC |<b>Experience Challenges Data Exploration</b><br><br> &bullet; Try out a few we've written or try your own! <br> &bullet; Navigate to SQL Workspace, Shared Workspace (folder) to view pre-created queries by dataset | <img src='https://raw.githubusercontent.com/BlueprintTechnologies/Databricks_Sql_Workshop/main/img_artifacts/query_shared_folder.png' />  |
